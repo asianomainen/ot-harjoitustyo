91467bd303dcbb2594d42f820af34f0737db5702
asianomainen <oskari.nuottonen@gmail.com>
2021-05-03 23:34:26 +0300

    Removes unnecessary variable in creating Polygon

--- SpaceInvaders/src/main/java/spaceinvadersapp/domain/EnemyShip.java
+++ SpaceInvaders/src/main/java/spaceinvadersapp/domain/EnemyShip.java
@@ -13,6 +13,6 @@ import javafx.scene.shape.Polygon;
 public class EnemyShip extends Shape {
 
     public EnemyShip(int x, int y, Color color) {
-        super(new Polygon(-20, 0, -20, -15, 0, -12, 20, -15, 20, 0, 15, 7, 10, 0, 5, 0, 0, 15, -5, 0, -10, 0, -15, 7, -20, 0), color, x, y);
+        super(new Polygon(-20, 0, -20, -15, 0, -12, 20, -15, 20, 0, 15, 7, 10, 0, 5, 0, 0, 15, -5, 0, -10, 0, -15, 7), color, x, y);
     }
 }
