65d3cbf9a4e91bb1f88ad0430154a6cbd1a20bf4
asianomainen <oskari.nuottonen@gmail.com>
2021-05-03 23:47:45 +0300

    Creates class for creating a boss enemy

---
+++ SpaceInvaders/src/main/java/spaceinvadersapp/domain/BossEnemyShip.java
@@ -0,0 +1,18 @@
+package spaceinvadersapp.domain;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+/**
+ * class for making a boss enemy ship
+ * extends abstract class Shape
+ *
+ * @see spaceinvadersapp.domain.Shape
+ */
+
+public class BossEnemyShip extends Shape {
+
+    public BossEnemyShip(int x, int y, Color color) {
+        super(new Polygon(-20, 10, -20, -5, 0, -10, 20, -5, 20, 10, 15, 5, 10, 10, 5, 5, 0, 10, -5, 5, -10, 10, -15, 5, -20, 10), color, x, y);
+    }
+}
