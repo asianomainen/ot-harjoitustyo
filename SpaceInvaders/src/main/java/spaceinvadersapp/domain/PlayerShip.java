5ed42312498614d8f488b7aa4cc8b7f25026df22
asianomainen <oskari.nuottonen@gmail.com>
2021-05-03 23:37:44 +0300

    Fixes outOfBound method

--- SpaceInvaders/src/main/java/spaceinvadersapp/domain/PlayerShip.java
+++ SpaceInvaders/src/main/java/spaceinvadersapp/domain/PlayerShip.java
@@ -15,4 +15,22 @@ public class PlayerShip extends Shape {
     public PlayerShip(int x, int y, Color color) {
         super(new Polygon(-20, 12, -20, 0, -17, -4, -3, -4, -1, -10, 1, -10, 3, -4, 17, -4, 20, 0, 20, 12), color, x, y);
     }
+
+    @Override
+    public void moveLeft() {
+        if (this.getShape().getTranslateX() < 20) {
+            return;
+        }
+
+        super.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        if (this.getShape().getTranslateX() > 94git 0) {
+            return;
+        }
+
+        super.moveRight();
+    }
 }
