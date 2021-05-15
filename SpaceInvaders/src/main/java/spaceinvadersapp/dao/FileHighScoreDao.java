d01329907769d6b97bea5c6eee709cb1d63d0535
asianomainen <oskari.nuottonen@gmail.com>
2021-05-15 23:55:03 +0300

    Fixes code style error

--- SpaceInvaders/src/main/java/spaceinvadersapp/dao/FileHighScoreDao.java
+++ SpaceInvaders/src/main/java/spaceinvadersapp/dao/FileHighScoreDao.java
@@ -7,11 +7,12 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FileHighScoreDao implements HighScoreDao {
     private Sheets sheetsService;
-    public String SPREADSHEET_ID = "1e9IFMWunUko426Z4eMbGNMGwWqPIPVMd3q1gWKos9IU";
+    private final String spreadsheetID = "1e9IFMWunUko426Z4eMbGNMGwWqPIPVMd3q1gWKos9IU";
 
     public FileHighScoreDao() {
         try {
@@ -24,11 +25,11 @@ public class FileHighScoreDao implements HighScoreDao {
 
     @Override
     public boolean addHighScore(String name, String time, String points) {
-        ValueRange appendBody = new ValueRange().setValues(Arrays.asList(Arrays.asList(name, time, points)));
+        ValueRange appendBody = new ValueRange().setValues(Collections.singletonList(Arrays.asList(name, time, points)));
 
         try {
             sheetsService.spreadsheets().values()
-                    .append(SPREADSHEET_ID, "A4", appendBody)
+                    .append(spreadsheetID, "A4", appendBody)
                     .setValueInputOption("RAW")
                     .setInsertDataOption("INSERT_ROWS")
                     .setIncludeValuesInResponse(true)
@@ -41,11 +42,10 @@ public class FileHighScoreDao implements HighScoreDao {
 
     @Override
     public BatchGetValuesResponse getHighScores() throws IOException {
-        List<String> ranges = Arrays.asList("E3:E12","F3:F12","G3:G12");
-        BatchGetValuesResponse readResult = sheetsService.spreadsheets().values()
-                .batchGet(SPREADSHEET_ID)
+        List<String> ranges = Arrays.asList("E3:E12", "F3:F12", "G3:G12");
+        return sheetsService.spreadsheets().values()
+                .batchGet(spreadsheetID)
                 .setRanges(ranges)
                 .execute();
-        return readResult;
     }
 }
