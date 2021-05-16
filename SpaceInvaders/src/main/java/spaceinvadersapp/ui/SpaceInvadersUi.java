eb6a1d2531f437aebce3705fa691aa700fc0675d
asianomainen <oskari.nuottonen@gmail.com>
2021-05-16 18:41:59 +0300

    Removes unused import

--- SpaceInvaders/src/main/java/spaceinvadersapp/ui/SpaceInvadersUi.java
+++ SpaceInvaders/src/main/java/spaceinvadersapp/ui/SpaceInvadersUi.java
@@ -15,7 +15,6 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
